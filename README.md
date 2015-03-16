69ee930cd50a14591aadff6a87ed62fe773c9c9e
pmelab <philipp.melab@gmail.com>
2015-03-16 13:29:35 +0100

    Moving to Github.

---
+++ README.md
@@ -0,0 +1,13 @@
+# DO[cker-fo]RK workspace template
+
+## VMWare provider troubleshooting:
+```
+vagrant up --provider vmware_fusion
+
+… Waiting for HGFS …
+
+yum install perl -y
+/usr/bin/vmware-config-tools.pl -d
+```
+
+
---
+++ vagrant/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if which pip >/dev/null; then
+  echo "python-pip already installed - skipping"
+else
+  yum install epel-release -y
+  yum install python-devel -y
+  yum install python-pip -y
+fi
+
+if which ansible >/dev/null; then
+  echo "ansible already installed - skipping"
+else
+  pip install markupsafe ansible
+fi
+
+if which git >/dev/null; then
+  echo "git already installed - skipping"
+else
+  yum install git -y
+fi
+
+ansible-galaxy install --no-deps --force --role-file=/vagrant/roles.yml --roles-path=/etc/ansible/roles
+ANSIBLE_ROLES_PATH=/etc/ansible/roles:/opt/roles ansible-playbook -i /vagrant/inventory.ini /vagrant/workstation.yml
